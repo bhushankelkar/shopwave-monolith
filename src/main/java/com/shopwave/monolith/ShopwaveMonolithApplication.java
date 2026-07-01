7d6570507a2147afb2cac9efe85b5b1cb356df21
bhushankelkar <kelkarkulbhushan@gmail.com>
2026-07-01 13:28:04 +0530

    Initial Spring Boot 3.5 skeleton (Java 21, Web/JPA/Postgres/Redis/Kafka/Security)

---
+++ src/main/java/com/shopwave/monolith/ShopwaveMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.shopwave.monolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopwaveMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopwaveMonolithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopwave/monolith/ShopwaveMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopwave.monolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopwaveMonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
